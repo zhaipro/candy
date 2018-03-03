ccd5f4ec79a10392b51bf6c281e57e2861b43222
yifuda <xin_soft@foxmail.com>
2018-03-03 12:06:04 +0800

    提取指定账号的电报消息

--- commands/telegram.py
+++ commands/telegram.py
@@ -1,19 +1,33 @@
 # coding: utf-8
 import argparse
 
+import telethon
+
 import orm
 import telegram
+import helpers
 
 
 def ping():
     for account in orm.TelegramAccount.select():
-        client = telegram.create_client(account.phone)
+        client = helpers.telegram_create_client(account.phone)
         telegram.ping(client)
+
+
+def fetch(phone):
+    client = helpers.telegram_create_client(phone)
+    for dialog in client.get_dialogs(limit=10):
+        print(telethon.utils.get_display_name(dialog.entity), dialog.message)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('cmd', choices=['ping'])
+    parser.add_argument('cmd', choices=['debug', 'ping', 'fetch'])
+    parser.add_argument('--phone')
     args = parser.parse_args()
     if args.cmd == 'ping':
         ping()
+    elif args.cmd == 'fetch':
+        fetch(args.phone)
+    else:
+        print(args)
